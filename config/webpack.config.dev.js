37c021ef6399c279ea41eeee23bd9feb95bd2ca4
night-crawler <lilo.panic@gmail.com>
2018-10-07 19:57:25 +0300

    added webpack-dashboard

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+
 const webpack = require('webpack');
 const PnpWebpackPlugin = require('pnp-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,11 +10,14 @@ const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
-const getClientEnvironment = require('./env');
-const paths = require('./paths');
 const ManifestPlugin = require('webpack-manifest-plugin');
+// eslint-disable-next-line
 const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+const paths = require('./paths');
+const getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -327,6 +331,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new DashboardPlugin(),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
