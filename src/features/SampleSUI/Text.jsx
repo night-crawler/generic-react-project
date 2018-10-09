f8f1acd9ee25d4e016fa7f64e4b069c4d41ee9cf
night-crawler <lilo.panic@gmail.com>
2018-10-09 12:42:55 +0300

    fixed code style

--- src/features/SampleSUI/Text.jsx
+++ src/features/SampleSUI/Text.jsx
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import { Form, Checkbox, Segment } from 'semantic-ui-react';
 
 export class Text extends React.Component {
-    static propTypes = {
-      uuid: PropTypes.string,
-      data: PropTypes.any,
-    };
+  static propTypes = {
+    uuid: PropTypes.string,
+    data: PropTypes.any,
+  };
 
-    constructor(props) {
-      super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-      return (
-        <Segment as={ Form } className='Text Block' color='brown'>
-          { this.props.uuid } - { this.props.data }
-          <Checkbox
-            label='EN > RU'
-            toggle={ true }
-          />
-        </Segment>
-      );
-    }
+  render() {
+    return (
+      <Segment as={ Form } className='Text Block' color='brown'>
+        { this.props.uuid } - { this.props.data }
+        <Checkbox
+          label='EN > RU'
+          toggle={ true }
+        />
+      </Segment>
+    );
+  }
 }
